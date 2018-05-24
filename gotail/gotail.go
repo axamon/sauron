61f8a75a04b4b7a4e184a84f5e3bf34b5a3cfba1
alberto <alberto@Host-001.homenet.telecomitalia.it>
2018-05-24 20:55:25 +0200

    funziona

--- gotail/gotail.go
+++ gotail/gotail.go
@@ -7,10 +7,13 @@ import (
 
 	"github.com/axamon/reperibili"
 
+	sms "github.com/axamon/sms"
+
 	"github.com/hpcloud/tail"
 )
 
-var nagioslog = flag.String("f", "/var/log/nagios/nagios.log", "Nagios file di log")
+var nagioslog = flag.String("nagioslog", "/var/log/nagios/nagios.log", "Nagios file di log")
+var reperibilita = flag.String("reperibilita", "$GOPATH/src/github.com/axamon/sauron/sauron/reperibilita.csv", "Nagios file di log")
 
 func main() {
 	flag.Parse()
@@ -36,17 +39,31 @@ func main() {
 	for line := range t.Lines {
 		//fmt.Println(line.Text)
 		//Se la linea è di notifica la analizza se no passa oltre
-		notificabool := strings.Contains(line.Text, "NOTIFICATION")
-		switch notificabool {
+		//notificabool := strings.Contains(line.Text, "NOTIFICATION")
+		switch {
 
-		case true:
+		case strings.Contains(line.Text, "NOTIFICATION") && strings.Contains(line.Text, "CRITICAL"):
 			fmt.Println(line.Text)
-			reperibile, _ := reperibili.Reperibiliperpiattaforma2("CDN", "reperibilita.csv")
+			//TODO cambiare CDN con qualcosa di variabile
+			reperibile, _ := reperibili.Reperibiliperpiattaforma2("CDN", *reperibilita)
 
 			TO := reperibile.Cellulare
 			NOME := reperibile.Nome
+			//debug
+			fmt.Println(TO, NOME)
 
 			reperibili.Chiamareperibile(TO, NOME)
+
+		case strings.Contains(line.Text, "NOTIFICATION") && strings.Contains(line.Text, "OK"):
+			//Se ok allora manda solo sms niente chiamata
+			fmt.Println("ricevuto OK")
+			reperibile, _ := reperibili.Reperibiliperpiattaforma2("CDN", *reperibilita)
+
+			TO := reperibile.Cellulare
+			pezzi := strings.Split(line.Text, ";")
+			messaggio := "Su " + pezzi[1] + " servizio " + pezzi[2] + " " + pezzi[3]
+
+			go sms.Inviasms(TO, messaggio)
 
 		default:
 			//fmt.Println("debug")
