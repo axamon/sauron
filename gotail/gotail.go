ce531ab8f677938b925dba8762b1ad218c907d3d
alberto <alberto@Host-001.homenet.telecomitalia.it>
2018-05-24 17:43:39 +0200

    recupera notifiche nagios

---
+++ gotail/gotail.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/hpcloud/tail"
+)
+
+var nagioslog = flag.String("f", "/var/log/nagios/nagios.log", "Nagios file di log")
+
+func main() {
+	flag.Parse()
+
+	//Inizia il tail dalla fine del file senza leggerlo dall'inizio
+	var fine tail.SeekInfo
+	fine.Offset = 0
+	fine.Whence = 2
+
+	//MustExist il file deve esistere Follow fa tail -f e ReOpen gestisce il logrotate
+	t, err := tail.TailFile(*nagioslog,
+		tail.Config{
+			Location:  &fine,
+			MustExist: true,
+			Follow:    true,
+			ReOpen:    true,
+		})
+
+	if err != nil {
+		fmt.Println("errore: ", err.Error())
+	}
+	//Per ogni nuova linea nel file
+	for line := range t.Lines {
+		//fmt.Println(line.Text)
+		//Se la linea è di notifica la analizza se no passa oltre
+		notificabool := strings.Contains(line.Text, "NOTIFICATION")
+		switch notificabool {
+
+		case true:
+			fmt.Println(line.Text)
+		default:
+			//fmt.Println("debug")
+			continue
+		}
+	}
+}
